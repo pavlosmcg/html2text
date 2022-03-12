496423f9a7db081551cbf79cb07136879852e19b
pavlosmcg <pavlosmcg@users.noreply.github.com>
2022-03-12 14:00:50 +1300

    Add conversion method

---
+++ src/Html.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Html2Text
+{
+    public static class Html
+    {
+        static string GetText(string input)
+        {
+            return "Hello, World";
+        }
+    }
+}
