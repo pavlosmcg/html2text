877f5ebe5121387864653397f0620e98389a1584
pavlosmcg <pavlosmcg@users.noreply.github.com>
2022-03-13 17:34:36 +1300

    Add console test app

---
+++ console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Html2Text;
+
+namespace Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var fileStream = new FileStream("clampdown.html", FileMode.Open, FileAccess.Read))
+            {
+                var result = Html.GetText(fileStream);
+                System.Console.WriteLine(result);
+            }
+        }
+    }
+}
--- src/HtmlConvertor.cs
+++ src/HtmlConvertor.cs
@@ -1,16 +1,17 @@
-﻿using Antlr4.Runtime;
+﻿using System.IO;
+using Antlr4.Runtime;
 using Html2Text.Antlr;
 
 namespace Html2Text
 {
     public static class Html
     {
-        public static string GetText(string html)
+        public static string GetText(Stream htmlStream)
         {
-            var inputStream = new AntlrInputStream(html);
+            var inputStream = new AntlrInputStream(htmlStream);
             var lexer = new HTMLLexer(inputStream);
             var tokenStream = new CommonTokenStream(lexer);
-            var parser = new HTMLParser(tokenStream);
+            var parser = new HTMLParser(tokenStream) { BuildParseTree = true };
             var documentContext = parser.htmlDocument();
 
             var visitor = new TextExtractionVisitor();
